7be9c5be45272c656119c0cd53d07c2b92f71121
Zorenko <denis.zorenko@kantar.com>
2018-05-28 11:05:31 +0200

    load_data with import_open and import_funnel

--- load_data.py
+++ load_data.py
@@ -7,7 +7,6 @@ from timeit import timeit
 mdd_path = 'D:\\ktv\\KTVONLINE_18_IMPORT.mdd'
 ddf_path = 'D:\\ktv\\KTVONLINE_18_IMPORT.ddf'
 category_map = {}
-records = []
 
 # sqlite - source db
 sqlite_conn = sqlite3.connect(ddf_path)
@@ -19,7 +18,6 @@ mssql_engine = create_engine(mssql_connection)
 mssql_conn = mssql_engine.connect().connection
 mssql_cursor = mssql_conn.cursor()
 
-
 def read_category_map():
     print('Reading category map...', end=' ')
     tree = et.parse(mdd_path)
@@ -28,10 +26,10 @@ def read_category_map():
     category_map = {int(m.attrib['value']): m.attrib['name'] for m in map_root}
     print('OK')
 
-
 def empty_import_tables():
     print('Emptying destination tables...', end=' ')
     mssql_cursor.execute('delete from import_open')
+    mssql_cursor.execute('delete from import_funnel')
     mssql_cursor.execute('delete from import_interviews')
     mssql_conn.commit()
     print('OK')
@@ -40,7 +38,6 @@ def empty_import_tables():
 def read_import_interviews():
     print('Reading interviews...', end=' ')
 
-    global records
     records = []
 
     result = sqlite_cursor.execute('''
@@ -61,40 +58,96 @@ def read_import_interviews():
             [weight:D] as weight
         from L1
         ''')
-
     for row in result:
-        record = (
-            row[0],  # serial
-            category_map[row[1]],  # fbgrot
-            row[2],  # start_time
-            row[3],  # age
-            category_map[row[4]],  # fa
-            category_map[row[5]],  # fb
-            category_map[row[6]],  # fc
-            category_map[row[7]],  # ff
-            category_map[row[8]],  # fg
-            category_map[row[9]],  # fha
-            category_map.get(row[10], 'na'),  # fhb
-            category_map[row[11]],  # f12a
-            category_map.get(row[12], 'na'),  # f12b
-            row[13]  # weight
-        )
-        stringified_record = str(record)
-        records.append(stringified_record)
+        serial = row[0]
+        fbgrot = category_map[row[1]]
+        start_time = row[2]
+        age = row[3]
+        fa = category_map[row[4]]
+        fb = category_map[row[5]]
+        fc = category_map[row[6]]
+        ff = category_map[row[7]]
+        fg = category_map[row[8]]
+        fha = category_map[row[9]]
+        fhb = category_map.get(row[10], 'na')
+        f12a = category_map[row[11]]
+        f12b = category_map.get(row[12], 'na')
+        weight = row[13]
+
+        record = str((serial, fbgrot, start_time, age,
+            fa, fb, fc, ff, fg, fha, fhb,
+            f12a, f12b, weight))
+        records.append(record)
     print('OK')
+    return records
+
+
+def read_import_funnel():
+    print('Reading funnel questions...', end=' ')
+
+    questions = 'f3g f5g f7g'.split()
+    records = []
+
+    result = sqlite_cursor.execute('''
+        select [Respondent.Serial:L],
+            [f3g:S], [f5g:S], [f7g:S]
+        from L1
+        ''')
+    for row in result:
+        serial = row[0]
+        for index, question in enumerate(questions, start=1):
+            brands = row[index]
+            if brands:
+                for brand_value in brands.split(';'):
+                    if brand_value:
+                        brand = category_map.get(int(brand_value))
+                        record = str((serial, question, brand))
+                        records.append(record)
+    print('OK')
+    return records
+
+
+def read_import_open():
+    print('Reading open questions...', end=' ')
+
+    questions = 'f1l f2l'.split()
+    records = []
+
+    tables = sqlite_cursor.execute('''
+        select TableName, DSCTableName
+        from Levels
+        where DSCTableName in ({0})
+    '''.format(','.join("'" + q + "'" for q in questions)))
+
+    for table in tables:
+        table_name = table[0]
+        question = table[1][:-1]
+        result = sqlite_cursor.execute('''
+            select [:P1], [LevelId:C1], [{0}:X]
+            from {1}
+            where [{0}:X] <> ''
+            '''.format(question, table_name))
+        for row in result:
+            serial = row[0]
+            iteration = int(category_map[row[1]][2:])
+            answer = row[2]
+            record = str((serial, question, iteration, answer))
+            records.append(record)
+        
+    print('OK')
+    return records
 
 
 def chunker(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 
-def write_import_interviews():
-    print('Inserting interviews...', end=' ')
+def write_records(records, table_name):
+    print('Inserting into {0}...'.format(table_name), end=' ')
     insert_header = '''
-        insert into import_interviews
-        (serial, fbgrot, start_time, age, fa, fb, fc, ff, fg, fha, fhb, f12a, f12b, weight)
+        insert into {0}
         values 
-    '''
+    '''.format(table_name)
     for batch in chunker(records, 1000):
         insert_values = ','.join(batch)
         insert_statement = insert_header + insert_values
@@ -106,8 +159,13 @@ def write_import_interviews():
 def main():
     read_category_map()
     empty_import_tables()
-    read_import_interviews()
-    write_import_interviews()
+    interviews = read_import_interviews()
+    funnel = read_import_funnel()
+    opens = read_import_open()
+
+    write_records(interviews, 'import_interviews')
+    write_records(funnel, 'import_funnel')
+    write_records(opens, 'import_open')
     print('Complete')
 
 
