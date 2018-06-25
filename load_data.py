22c144c624a43805c7b528bb22511a519afa22e1
Zorenko <denis.zorenko@kantar.com>
2018-06-25 21:44:20 +0200

    180625

--- load_data.py
+++ load_data.py
@@ -7,6 +7,7 @@ from enums import openConstants, DataTypeConstants
 from win32com import client
 import sqlite3
 from sqlalchemy import create_engine
+from collections import defaultdict
 
 # sqlite - source db
 sqlite_conn = sqlite3.connect(sqlite_connection)
@@ -88,7 +89,7 @@ def read_import_funnel():
         from L1
         ''')
     for row in result:
-        serial = row[0] + settings.serial_increment * 1000000
+        serial = row[0] + settings.serial_increment * 1_000_000
         for index, question in enumerate(questions, start=1):
             brands = row[index]
             if brands:
@@ -121,7 +122,7 @@ def read_import_fc():
         '''.format(table_name))
 
     for row in result:
-        serial = row[0] + settings.serial_increment * 1000000
+        serial = row[0] + settings.serial_increment * 1_000_000
         category = settings.category_map[row[1]]
         brand = settings.category_map[row[2]]
         record = str((serial, category, brand))
@@ -130,39 +131,7 @@ def read_import_fc():
     print('OK')
     return records
 
-def read_import_open_old():
-    print('Reading open questions...', end=' ')
-
-    questions = 'f1l f2l'.split()
-    records = []
-
-    tables = sqlite_cursor.execute('''
-        select TableName, DSCTableName
-        from Levels
-        where DSCTableName in ({0})
-    '''.format(','.join("'" + q + "'" for q in questions)))
-
-    for table in tables:
-        table_name = table[0]
-        question = table[1][:-1]
-        result = sqlite_cursor.execute('''
-            select b.[Respondent.Serial:L], a.[LevelId:C1], a.[{0}:X]
-            from {1} as a
-                join L1 as b
-                    on a.[:P1] = b.[:P0]
-            where a.[{0}:X] <> ''
-            '''.format(question, table_name))
-        for row in result:
-            serial = row[0] + settings.serial_increment * 1000000
-            question = table[1] + '[{' + settings.category_map[row[1]] + '}].' + table[1][:-1]
-            answer = row[2].replace("'", "#####")
-            record = str((serial, question, answer)).replace("#####", "''")
-            records.append(record)
-        
-    print('OK')
-    return records
-
-def read_import_open():
+def read_import_open_vdata():
     print('Reading open questions...', end=' ')
 
     # reading ignored variables from db
@@ -177,7 +146,6 @@ def read_import_open():
             if not v.FullName in ignored_variables:
                 mdm_variables.append(v.FullName)
     mdd.Close()
-
     
     ddf.Open()
     rs, _ = ddf.Execute('select Respondent.Serial, {0} from vdata'.format(','.join(mdm_variables)))
@@ -188,7 +156,7 @@ def read_import_open():
         for f in rs.Fields:
             answer = None
             if f.Name == 'Respondent.Serial':
-                serial = int(f.Value) + settings.serial_increment * 1000000
+                serial = int(f.Value) + settings.serial_increment * 1_000_000
             else:
                 answer = f.Value
             if answer:
@@ -203,6 +171,108 @@ def read_import_open():
     print('OK')
     return records
 
+def read_import_open():
+    print('Reading open questions...', end = ' ')
+
+    # reading levels 
+    levels = {l[0] : [l[1], [l[2]]] for l in sqlite_cursor.execute('''
+        select TableName, DSCTableName, ParentName
+        from Levels''').fetchall()}
+    for v in levels.values():
+        parent_tables = v[1]
+        parent_table = parent_tables[0]
+        while parent_table:
+            parent_table = levels[parent_table][1][0]
+            parent_tables.append(parent_table)
+    levels = {k: [v[0], v[1][:-1]] for k, v in levels.items()}
+
+    # reading ignored variables from db
+    ignored_variables = set(v[0] for v in mssql_engine.execute('''select variable
+        from open_variables where type = ?''', 'ignored').fetchall())
+
+    # reading all text variables from mdd and builds flat list (VDATA) of variables
+    flat_variables = []
+    mdd = client.Dispatch('MDM.Document')
+    mdd.Open(mdd_path,mode=openConstants.oREAD)
+    for v in mdd.Variables:
+        if v.DataType == DataTypeConstants.mtText and not v.IsSystemVariable \
+        and v.HasCaseData and v.FullName not in ignored_variables:
+            flat_variables.append(v.FullName)
+    mdd.Close()
+
+    # build a dictionary of variables with corresponding level and field name on that level
+    # e.g. f1l[{c_1}].f1 -> f1l.f1
+    variables = {}
+    for fv in flat_variables:
+        v = '.'.join(part.split('[')[0] for part in fv.split('.'))
+        variables[v] = ['L1', '']
+   
+    # assign levels to variable
+    for va in variables:
+        variables[va][1] = va
+        parts = va.split('.')        
+        for i in range(len(parts) - 1):
+            prefix = parts[i]
+            suffix = '.'.join(parts[i + 1:])
+            for k, v in levels.items():
+                if prefix == v[0]:
+                    variables[va][0] = k
+                    variables[va][1] = suffix
+                    break
+        
+    # build tables dictionary containing list of text variables in this table
+    tables = defaultdict(list)
+    for v, l in variables.items():
+        tables[l[0]].append(l[1])
+    
+    # relevant tables: tables containing text variables + their parent tables
+    relevant_tables = {}
+    for t in tables.keys():
+        relevant_tables.update({l : {} for l in levels[t][1] + [t]})
+
+    for t in relevant_tables.keys():
+        all_columns = [r[1] for r in sqlite_cursor.execute(f'pragma table_info({t})').fetchall()]
+        id_columns = [f'[{c}]' for c in all_columns if c[:2] == ':P']
+        if 'LevelId:C1' in all_columns:
+            values_dict = {row[:-1]: row[-1] for row in sqlite_cursor.execute(f'''
+                select {','.join(id_columns)}, [LevelId:C1]
+                from {t}''').fetchall()}
+            relevant_tables[t] = values_dict
+
+    # builds sql queries per table
+    records = []
+    for t in tables:
+        # get column list
+        all_columns = sqlite_cursor.execute(f'pragma table_info({t})').fetchall()
+        id_columns = [f'[{c[1]}]' for c in all_columns if c[1][:2] == ':P']
+        text_columns = [f'[{c}:X]' for c in tables[t]]
+        query_columns = id_columns + text_columns
+
+        # level_tables contains the table itself and all its parents
+        # number of id columns should match number of level tables
+        level_tables = levels[t][1][::-1] + [t]
+        assert len(id_columns) == len(level_tables)
+        
+        #builds and executes sql
+        for row in sqlite_cursor.execute(f'''select {','.join(query_columns)} from {t}'''):
+            serial = row[0] + settings.serial_increment * 1_000_000
+            variable_prefix = ''
+            for i in range(1, len(level_tables)):
+                field = levels[level_tables[i]][0]
+                iteration_value = relevant_tables[level_tables[i]][row[:i + 1]]
+                iteration_category = settings.category_map[iteration_value]
+                variable_prefix += f'{field}[{{{iteration_category}}}].'
+            for i in range(len(text_columns)):    
+                offset = i + len(id_columns)
+                variable = variable_prefix + text_columns[i][1:-3]
+                answer = row[offset]
+                if answer:
+                    answer = answer.replace("'", "''")
+                    record = '(' + str(serial) + ", '" + variable + "', '" + answer + "')"
+                    records.append(record)
+    print('OK')
+    return records
+   
 def chunker(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
@@ -237,4 +307,6 @@ if __name__ == '__main__':
     import prepare_data
     initialize.main()
     prepare_data.main()
-    main()
+
+    from timeit import timeit
+    print(timeit(main,number=1))
--- main.py
+++ main.py
@@ -3,12 +3,13 @@
 import initialize
 import prepare_data
 import load_data
-# from process_open import main as process_open
+import process_open
 # from create_outputs import main as create_outputs
 
 initialize.main()
 prepare_data.main()
 load_data.main()
+process_open.main()
 
 # load_data()
 # process_open()
